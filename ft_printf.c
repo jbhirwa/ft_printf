f31e7bbd0cee04dcfd8b96dd3bc63d5fd504e7ad
John Habaguhirwa <jhabaguh@F3C023.42adel.org.au>
2021-10-20 15:46:04 +1030

    ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 
-staatic t_va *memalloc(void)
+static t_va *memalloc(void)
 {
 	t_va *arg_struct;
 
@@ -11,7 +11,26 @@ staatic t_va *memalloc(void)
 	return (arg_struct);
 }
 
-// converter;
+void	converter(const char c, arg_struct)
+{
+	if (c == 'i' || c == 'd')
+		isint(arg_struct);
+	else if (c == 'c')	
+		ischar(arg_struct);
+	else if (c == 's')
+		isstring(arg_struct);
+	else if (c == 'p')
+		ispointer(arg_struct);
+	else if (c == 'u')
+		isunint(arg_struct);
+	else if (c == 'x' || c == 'X')
+		ishex(arg_struct, c);
+	else id (c == '%')
+	{
+		ft_putchar_fd(c, 1);
+		arg_struct->count++;
+	}
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -23,6 +42,14 @@ int	ft_printf(const char *format, ...)
 	format_len = 0;
 	arg_struct = memalloc();// memory allocation for arg_struct;
 	va_start(arg_struct->args, str);
-//	while loop (if )
-	
+	while (format[i])
+	{
+		if (format[i] == '%')
+			converter(format[++i], arg_struct)
+		else
+		{
+			ft_putchar_fd(str[i], 1);
+			arg_struct->counter++;
+		}
+	}	
 }
